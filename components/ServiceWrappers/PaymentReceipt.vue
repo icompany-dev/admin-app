7a9841cfefff75805246ef437a58e72b1e717330
bahiyah <bahiyah@icompany.my>
2026-07-20 16:10:24 +0800

    add payment receipt

---
+++ components/ServiceWrappers/PaymentReceipt.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="service-wrappers-payment-receipt">
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { PaymentReceiptController } from '~/scripts/components/service-wrappers/PaymentReceiptController'
+
+const props = defineProps({})
+const emit = defineEmits([]) 
+
+const controller = new PaymentReceiptController(props, emit)
+</script>
+
+<style lang="scss">
+@use '~/assets/scss/components/ServiceWrappers/PaymentReceipt' as *;
+</style>
