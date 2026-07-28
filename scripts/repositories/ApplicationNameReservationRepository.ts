824bff8e27eaace28eb47cdab8b828d0ac959dbb
bahiyah <bahiyah@icompany.my>
2026-07-28 18:38:16 +0800

    fix data to backend

--- scripts/repositories/ApplicationNameReservationRepository.ts
+++ scripts/repositories/ApplicationNameReservationRepository.ts
@@ -11,23 +11,50 @@ export class ApplicationNameReservationRepository extends Repository<Application
     super(resourceUrl, singleResourceUrl, baseUrl, getAuthToken, ApplicationNameReservation)
   }
 
-  async approve(id: string): Promise<any> {
+  async submitted(incorporationId: string, data: any): Promise<any> {
     try {
-      const response = this.post(`${this.singleResourceUrl}/approved/${id}`, {})
+      const response = this.post(`${this.singleResourceUrl}/submitted/${incorporationId}`, data)
       return response
     } catch (e) {
       throw e
     }
   }
 
-  async reject(id: string, reason: string): Promise<any> {
+  async queried(incorporationId: string, data: any): Promise<any> {
+    try {
+      const response = this.post(`${this.singleResourceUrl}/queried/${incorporationId}`, data)
+      return response
+    } catch (e) {
+      throw e
+    }
+  }
+
+  async resubmitted(incorporationId: string, data: any): Promise<any> {
+    try {
+      const response = this.post(`${this.singleResourceUrl}/resubmitted/${incorporationId}`, data)
+      return response
+    } catch (e) {
+      throw e
+    }
+  }
+
+  async approved(incorporationId: string, data: object): Promise<any> {
+    try {
+      const response = this.post(`${this.singleResourceUrl}/approved/${incorporationId}`, data)
+      return response
+    } catch (e) {
+      throw e
+    }
+  }
+
+  async rejected(incorporationId: string, reason: string): Promise<any> {
     try {
       let data = {
         ssm_remarks_en: reason,
         ssm_remarks_bm: reason,
       }
 
-      const response = this.post(`${this.singleResourceUrl}/rejected/${id}`, data)
+      const response = this.post(`${this.singleResourceUrl}/rejected/${incorporationId}`, data)
       return response
     } catch (e) {
       throw e
--- stores/ApplicationNameReservations.ts
+++ stores/ApplicationNameReservations.ts
@@ -18,12 +18,12 @@ export const useApplicationNameReservationStore = defineStore("applicationNameRe
     error: error,
   })
 
-  async function approve(id: string): Promise<ApplicationNameReservation | null> {
+  async function submitted(incorporationId: string, data: object): Promise<ApplicationNameReservation | null> {
     isLoading.value = true
     error.value = null
 
     try {
-      const response = await $repositories.companyNameReservations.approve(id)
+      const response = await $repositories.applicationNameReservations.submitted(incorporationId, data)
       return response.data ? new ApplicationNameReservation(response.data) : null
     } catch (e) {
       error.value = `Failed to update data: ${e}`
@@ -33,12 +33,57 @@ export const useApplicationNameReservationStore = defineStore("applicationNameRe
     }
   }
 
-  async function reject(id: string, reason: string): Promise<ApplicationNameReservation | null> {
+  async function queried(incorporationId: string, data: object): Promise<ApplicationNameReservation | null> {
     isLoading.value = true
     error.value = null
 
     try {
-      const response = await $repositories.companyNameReservations.reject(id, reason)
+      const response = await $repositories.applicationNameReservations.queried(incorporationId, data)
+      return response.data ? new ApplicationNameReservation(response.data) : null
+    } catch (e) {
+      error.value = `Failed to update data: ${e}`
+      throw error.value
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  async function resubmitted(incorporationId: string, data: object): Promise<ApplicationNameReservation | null> {
+    isLoading.value = true
+    error.value = null
+
+    try {
+      const response = await $repositories.applicationNameReservations.resubmitted(incorporationId, data)
+      return response.data ? new ApplicationNameReservation(response.data) : null
+    } catch (e) {
+      error.value = `Failed to update data: ${e}`
+      throw error.value
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  async function approve(incorporationId: string, data: object): Promise<ApplicationNameReservation | null> {
+    isLoading.value = true
+    error.value = null
+
+    try {
+      const response = await $repositories.applicationNameReservations.approved(incorporationId, data)
+      return response.data ? new ApplicationNameReservation(response.data) : null
+    } catch (e) {
+      error.value = `Failed to update data: ${e}`
+      throw error.value
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  async function reject(incorporationId: string, reason: string): Promise<ApplicationNameReservation | null> {
+    isLoading.value = true
+    error.value = null
+
+    try {
+      const response = await $repositories.applicationNameReservations.rejected(incorporationId, reason)
       return response.data ? new ApplicationNameReservation(response.data) : null
     } catch (e) {
       error.value = `Failed to update data: ${e}`
@@ -57,6 +102,9 @@ export const useApplicationNameReservationStore = defineStore("applicationNameRe
     error,
     totalApplicationNameReservations,
     ...crudActions,
+    submitted,
+    resubmitted,
+    queried,
     approve,
     reject,
   }
