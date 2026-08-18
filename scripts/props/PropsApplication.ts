addc0cbc08233dec1b605d583eaac325a7cbbb88
bahiyah <bahiyah@icompany.my>
2026-08-18 19:43:35 +0800

    add id

--- scripts/props/PropsApplication.ts
+++ scripts/props/PropsApplication.ts
@@ -1,11 +1,14 @@
 export interface IPropsApplication {
   companyId: string
+  applicationId: string | null
 }
 
 export class PropsApplication implements IPropsApplication {
   companyId: string
+  applicationId: string | null = null
 
-  constructor(companyId: string) {
+  constructor(companyId: string, applicationId: string | null = null) {
     this.companyId = companyId
+    this.applicationId = applicationId
   }
 }
