031a7bc0572edb13583f884eace956ec4d647006
bahiyah <bahiyah@icompany.my>
2026-09-07 11:02:47 +0800

    add director appointment page

---
+++ components/SecretarialServices/AppointDirector.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="secretarial-services-appoint-director">
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { AppointDirectorController } from '~/scripts/components/secretarial-services/AppointDirectorController'
+
+const props = defineProps({})
+const emit = defineEmits([]) 
+
+const controller = new AppointDirectorController(props, emit)
+</script>
+
+<style lang="scss">
+@use '~/assets/scss/components/SecretarialServices/AppointDirector' as *;
+</style>
