5b247200cd7782d194e209275ff9c5091a4a6110
bahiyah <bahiyah@icompany.my>
2026-09-17 15:17:53 +0800

    add tags

---
+++ scripts/components/popups/AddNewTagController.ts
@@ -0,0 +1,121 @@
+import { Tag } from "~/scripts/models/Tag"
+import type { PropsAddNewTag } from "~/scripts/props/PropsAddNewTag"
+import { BasePopupController } from "./BasePopupController"
+import { Error } from "~/scripts/library/Error"
+import { PopupTitles, PopupTitlesBm } from "~/scripts/constants/Popups"
+import { Filter } from "~/scripts/library/Filter"
+import { EmitMessages } from "~/scripts/constants/EmitMessages"
+
+export class AddNewTagController extends BasePopupController {
+  targetType: Ref<string> = ref<string>("")
+  targetId: Ref<string> = ref<string>("")
+  name: Ref<string> = ref<string>("")
+
+  isSubmitting: Ref<boolean> = ref<boolean>(false)
+
+  isAddNewType: Ref<boolean> = ref<boolean>(false)
+
+  tags: Ref<Tag[]> = ref<Tag[]>([])
+
+  constructor(props: PropsAddNewTag, emitEvents: any) {
+    super(emitEvents)
+
+    this.isCompliance.value = false
+
+    this.setDataFromProps(props)
+  }
+
+  setDataFromProps(props: PropsAddNewTag): void {
+    this.targetType.value = props.targetType
+    this.targetId.value = props.targetId
+  }
+
+  async fetchExistingTags(): Promise<void> {
+    let filter = new Filter()
+    filter.takeAll = true
+
+    let repository = useTagStore()
+    let response = await repository.fetchAll(filter)
+    this.tags.value = response.data.map((d: any) => {
+      return new Tag(d)
+    })
+  }
+
+  onSelectedValue(): void {
+    this.isAddNewType.value = this.name.value === "+ Add New"
+
+    if (this.isAddNewType.value) {
+      this.name.value = ""
+    }
+  }
+
+  onClearCustomName(): void {
+    this.name.value = ""
+    this.isAddNewType.value = false
+  }
+
+  onProceedClicked(): void {
+    //
+  }
+
+  async onSave(): Promise<void> {
+    try {
+      this.isSubmitting.value = true
+
+      let tag = new Tag()
+      tag.targetType = this.targetType.value
+      tag.targetId = this.targetId.value
+      tag.name = this.name.value
+
+      await tag.create(useTagStore())
+
+      this.emitEvents(EmitMessages.PROCEED, tag)
+    } catch (e) {
+      if (e instanceof Error) {
+        e.handle()
+      } else {
+        let error = new Error()
+        error.setForCUD()
+        error.handle()
+      }
+    } finally {
+      this.isSubmitting.value = false
+    }
+  }
+
+  get title(): string {
+    return this.language.isMalay() ? PopupTitlesBm.ImportantNotice : PopupTitles.ImportantNotice
+  }
+
+  get heading(): string {
+    return this.language.isMalay() ? `Aksi dalam Progres` : `Action in Progress`
+  }
+
+  get cta(): string {
+    return this.language.isMalay() ? "Ingin teruskan?" : "Would you like to continue?"
+  }
+
+  get content(): string {
+    if (this.language.isMalay()) {
+      return `
+       Sila lengkapkan butiran dibawah.
+      `
+    }
+
+    return `
+      Please complete the detail below.
+    `
+  }
+
+  get existingTags(): string[] {
+    let tagNames = this.tags.value.map((t: Tag) => {
+      return t.name
+    })
+
+    let distinctNames = [...new Set(tagNames)]
+
+    distinctNames.push("+ Add New")
+
+    return distinctNames
+  }
+}
--- scripts/constants/EmitMessages.ts
+++ scripts/constants/EmitMessages.ts
@@ -11,6 +11,7 @@ export class EmitMessages {
   static NO_EXISTING_APPLICATION: string = "noExistingApplication"
   static PAY: string = "pay"
   static PROCEED: string = "proceed"
+  static REFRESH: string = "refresh"
   static RESIGN_AS_DIRECTOR: string = "resignAsDirector"
   static RESERVE_NAME_FIRST: string = "reserveNameFirst"
   static SSM_CORPORATE_PROFILE: string = "ssmCorporateProfile"
