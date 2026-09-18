429ff50495b5c06b10e3cfd62e225a658adfe411
bahiyah <bahiyah@icompany.my>
2026-09-18 11:06:48 +0800

    fix emit message

--- components/Services/ChangeOfNameApplication.vue
+++ components/Services/ChangeOfNameApplication.vue
@@ -356,10 +356,11 @@
   import ServiceApplication from "./ServiceApplication.vue"
   import { ChangeOfNameApplicationController } from "~/scripts/components/services/ChangeOfNameApplicationController"
   import type { IPropsApplication } from "~/scripts/props/PropsApplication"
+  import { EmitMessages } from "~/scripts/constants/EmitMessages"
 
   const props = defineProps<IPropsApplication>()
 
-  const emit = defineEmits(["applicationId", "paymentOrderId", "pa", "documentSelected", "download", "show", "hide"])
+  const emit = defineEmits(EmitMessages.APPLICATION_SERVICES)
 
   const resolutionsRef = ref(null)
   const nameReservationRejectedPopup = ref(null)
