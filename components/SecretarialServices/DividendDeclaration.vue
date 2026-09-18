6d8a0020a593ca718ee6bf8646eb0c9809695952
bahiyah <bahiyah@icompany.my>
2026-09-18 10:08:11 +0800

    add dividend declaration individual page

---
+++ components/SecretarialServices/DividendDeclaration.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    id="secretarial-services-dividend-declaration"
+    class="secretarial-service-application"
+  >
+    <div class="service-application">
+      <!-- <ChangeOfNameApplication
+        v-bind="controller.applicationProps"
+        @company="controller.onCompanyUpdated($event)"
+        @paymentOrderId="controller.onPaymentOrderIdUpdated($event)"
+        @documentSelected="controller.onDocumentTargetSelected($event)"
+        @download="controller.onDownloadClicked()"
+      /> -->
+    </div>
+    <div class="document-container">
+      <component
+        ref="documentRef"
+        :is="activeDocumentComponent"
+        :company-id="controller.companyId.value"
+        :view-type="'existing'"
+        :application-id="controller.applicationId.value"
+        :target-id="controller.paymentOrderId.value"
+        :target-type="controller.target"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import ReceiptInvoiceService from "../CompanyServices/ReceiptInvoiceService.vue"
+  import { DividendDeclarationController } from "~/scripts/components/secretarial-services/DividendDeclarationController"
+  import type { IPropsSecretarialService } from "~/scripts/props/PropsSecretarialService"
+  import { DocumentTargets } from "~/scripts/constants/DocumentTargets"
+
+  const props = defineProps<IPropsSecretarialService>()
+
+  const emit = defineEmits(["company", "paymentOrderId"])
+
+  const documentRef = ref(null)
+
+  const controller = new DividendDeclarationController(props, emit)
+
+  const componentMap: Record<string, any> = {
+    // [DocumentTargets.TARGET_AMENDMENT_NAME_RESOLUTIONS]: ChangeOfNameService,
+    // [DocumentTargets.TARGET_AMENDMENT_NAME_SECTION27]: Section27Service,
+    // [DocumentTargets.TARGET_AMENDMENT_NAME_SECTION28]: Section28Service,
+    [DocumentTargets.TARGET_RECEIPT]: ReceiptInvoiceService,
+  }
+
+  const activeDocumentComponent = computed(() => {
+    const target = controller.selectedDocumentTarget.value
+    return target && componentMap[target] ? componentMap[target] : null
+  })
+
+  watch(
+    documentRef,
+    (newVal) => {
+      controller.setDocumentRef(newVal)
+    },
+    { immediate: true }
+  )
+</script>
+
+<style lang="scss">
+  @use "~/assets/scss/components/SecretarialServices/SecretarialService" as *;
+  @use "~/assets/scss/components/SecretarialServices/DividendDeclaration" as *;
+</style>
